5d92f76f49968a27c4b5d718866ead475c02ff5d
jsmonet <jsmonet@gmail.com>
2017-10-23 17:02:28 -0700

    setting the skeleton.

---
+++ scheck.go
@@ -0,0 +1,20 @@
+package main
+
+import "flag"
+
+var (
+	checkType      = flag.String("type", "", "What kind of check?")
+	hostAddress    = flag.String("host", "", "Enter a host address")
+	portNumber     = flag.Int("port", 22, "Enter a TCP port number")
+	timeOutSeconds = flag.Int("timeout", 5, "Enter a timeout in seconds")
+	volumeLocation = flag.String("vol", "/", "Enter a volume to check (disk space)")
+	volSizeWarn    = flag.Float64("volwarn", 75, "Percentage full that triggers a warning")
+	volSizeCrit    = flag.Float64("volcrit", 90, "Percentage full that triggers a critical")
+	authString     = flag.String("auth", "Z3Vlc3Q6Z3Vlc3Q=", "Curl Auth string. Default parses to guest:guest")
+)
+
+func main() {
+	// Parse for great justice
+	flag.Parse()
+
+}
